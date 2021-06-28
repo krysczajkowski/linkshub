9d073db8e94e450031e17b718185d6ff6831c5a5
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2021-06-28 22:04:33 +0200

    Settings edit crop image v2

--- linkshub/static/js/edit.js
+++ linkshub/static/js/edit.js
@@ -4,23 +4,16 @@ const submitBtn = document.getElementById('submitBtn')
 const imgPreview = document.getElementById('imgPreview')
 const imageField = document.getElementById('imageField')
 
-imageField.addEventListener('change', (e)=>{
-    console.log('adf')
-    const [file] = imageField.files
-
-    if (file) {
-        imgPreview.src = URL.createObjectURL(file)
-    }
-})
-
 // Image validation
+
+// image-box is the id of the div element that will store our cropping image preview
+const imagebox = document.getElementById('image-box')
+    // crop-btn is the id of button that will trigger the event of change original file with cropped file.
+const crop_btn = document.getElementById('crop-btn')
+
 const imageFeedback = document.querySelector('.image-feedback')
 
-imageField.addEventListener('change', validateFile)
-
-function validateFile(){
-    console.log('onchange')
-
+imageField.addEventListener('change', () => {
     imageFeedback.classList.remove('is-invalid')
     imageFeedback.classList.remove('is-valid')
     imageFeedback.style.display = 'none'
@@ -29,7 +22,7 @@ function validateFile(){
           sizeLimit = 5000000; // 5 megabyte
 
     // destructuring file name and size from file object
-    const { name:fileName, size:fileSize } = this.files[0];
+    const { name:fileName, size:fileSize } = imageField.files[0];
 
     const fileExtension = fileName.split(".").pop();
 
@@ -62,9 +55,60 @@ function validateFile(){
 
         imageField.classList.remove('is-invalid')
         imageField.classList.add('is-valid')
+
+        // Getting image file object from the input variable
+        const img_data = imageField.files[0]
+        // createObjectURL() static method creates a DOMString containing a URL representing the object given in the parameter.
+        // The new object URL represents the specified File object or Blob object.
+        const url = URL.createObjectURL(img_data)
+
+        // Creating a image tag inside imagebox which will hold the cropping view image(uploaded file) to it using the url created before.
+        imagebox.innerHTML = `<img src="${url}" id="image" style="width:100%;">`
+
+        // Storing that cropping view image in a variable
+        const image = document.getElementById('image')
+
+        // Creating a croper object with the cropping view image
+        // The new Cropper() method will do all the magic and diplay the cropping view and adding cropping functionality on the website
+        // For more settings, check out their official documentation at https://github.com/fengyuanchen/cropperjs
+        const cropper = new Cropper(image, {
+            aspectRatio: 1 / 1,
+            autoCropArea: 1,
+            viewMode: 1,
+            scalable: false,
+            zoomable: false,
+            movable: false,
+            minCropBoxWidth: 100,
+            minCropBoxHeight: 100,
+        })
+
+        // When crop button is clicked this event will get triggered
+        crop_btn.addEventListener('click', ()=>{
+        // This method coverts the selected cropped image on the cropper canvas into a blob object
+        cropper.getCroppedCanvas().toBlob((blob)=>{
+            
+            // Gets the original image data
+            let fileInputElement = document.getElementById('imageField');
+            // Make a new cropped image file using that blob object, image_data.name will make the new file name same as original image
+            let file = new File([blob], img_data.name,{type:"image/*", lastModified:new Date().getTime()});
+            // Create a new container
+            let container = new DataTransfer();
+            // Add the cropped image file to the container
+            container.items.add(file);
+            // Replace the original image file with the new cropped image file
+
+            fileInputElement.files = container.files;
+
+
+            imgPreview.src = URL.createObjectURL(file)
+
+            });
+        });
+
     }
 
-}
+})
+
 
 // Username validation
 /*
